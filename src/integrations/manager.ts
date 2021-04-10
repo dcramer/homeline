f889b1c197f525a27ea65efa83f1ca136be89296
David Cramer <dcramer@gmail.com>
2021-04-09 20:52:05 -0700

    Improve message

--- src/integrations/manager.ts
+++ src/integrations/manager.ts
@@ -21,7 +21,7 @@ export default class IntegrationManger {
 
   async start() {
     this.integrations.forEach(async (integration) => {
-      this.logger.info(`Registering integration ${integration.id}`);
+      this.logger.info(`Starting integration ${integration.id}`);
       try {
         // TODO(dcramer): need to make this check if the integration is alive
         try {
